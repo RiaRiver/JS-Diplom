3ac0170fe59f7ca9f9138292ad8a949413f9d56c
RiaRiver <riria.river@gmail.com>
2020-09-25 01:06:23 +0300

    feat: init consultation popup

--- src/index.js
+++ src/index.js
@@ -33,3 +33,14 @@ const  popupPrivacy = new PopUp({
   }
  `
 });
+
+const popupConsultation = new PopUp({
+  popUpSelector: '[data-popup="consultation"]',
+  openButtonsSelector: '[data-open="consultation"]',
+  closeButtonsSelector: '[data-close="consultation"]',
+  activeCSS: `
+  [data-popup="consultation"].active {
+  visibility: visible;
+  }
+ `
+});
