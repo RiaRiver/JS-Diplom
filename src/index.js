721386cba5da051d1b72b43c2c878ede076325f2
RiaRiver <riria.river@gmail.com>
2020-09-25 00:56:17 +0300

    feat: init menu popup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "./modules/polyfills";
 import { viewSecondPhoneNumber } from "./modules/viewSecondPhoneNumber";
+import { PopUp } from "./modules/PopUp";
 
 viewSecondPhoneNumber();
+
+// Инициализация моадльных окон
+const popupMenu = new PopUp({
+  popUpSelector: '[data-popup="menu"]',
+  closeButtonsSelector: '[data-close="menu"]',
+  openButtonsSelector: '[data-open="menu"]',
+  activeClass: 'showHide-menu',
+});
