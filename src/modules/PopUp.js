92c2be017591eedfb727a17aa9cfb8191ffce145
RiaRiver <riria.river@gmail.com>
2020-09-25 00:44:44 +0300

    feat: add PopUp class

---
+++ src/modules/PopUp.js
@@ -0,0 +1,40 @@
+// PopUp
+import { addStyle } from "./styles";
+
+export class PopUp {
+  constructor({ popUpSelector, openButtonsSelector, closeButtonsSelector, activeClass = 'active', activeCSS }) {
+    this.popUp = document.querySelector(popUpSelector);
+    this.openButtons = document.querySelectorAll(openButtonsSelector);
+    this.closeButtons = this.popUp.querySelectorAll(closeButtonsSelector);
+    this.activeClass = activeClass;
+
+    if (activeCSS) {
+      addStyle('js-popup-styles', activeCSS);
+    }
+    this.init();
+  }
+
+  // Открытие модального окна
+  openPopUp() {
+    this.popUp.classList.add(this.activeClass);
+  }
+
+  // Закрытие модального окна
+  closePopUp() {
+    this.popUp.classList.remove(this.activeClass);
+  }
+
+  // Слушатели на управляющие кнопки
+  init() {
+    if (this.openButtons.length) {
+      this.openButtons.forEach(button => {
+        button.addEventListener('click', this.openPopUp.bind(this));
+      });
+    }
+    if (this.closeButtons.length) {
+      this.closeButtons.forEach(button => {
+        button.addEventListener('click', this.closePopUp.bind(this));
+      });
+    }
+  }
+}
